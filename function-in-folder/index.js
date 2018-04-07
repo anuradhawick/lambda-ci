61a355eab397fdf098eb2ab6314caed9276ba98c
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2018-04-07 18:39:23 +0530

    commit

--- function-in-folder/index.js
+++ function-in-folder/index.js
@@ -1,12 +1,6 @@
 const { generate } = require('unique-names-generator');
 
 exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            display: "Folder functions invoked" + generate(),
-        }),
-    };
     
-    callback(null, response);
+    callback(null, "This is a sample function");
 };
