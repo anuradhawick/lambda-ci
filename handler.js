964a9a1c1bbf9bec0e969bc4b83186761badd4eb
Anuradha Wickramarachchi <anuradhawick@gmail.com>
2018-04-07 16:53:25 +0530

    commit

--- handler.js
+++ handler.js
@@ -1,12 +1,4 @@
-const { generate } = require('unique-names-generator');
-
 exports.handler = (event, context, callback) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            name: generate(),
-        }),
-    };
     
-    callback(null, response);
+    callback(null, "FUNCTION 2");
 };
